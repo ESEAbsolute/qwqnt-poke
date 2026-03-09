5b38c1977d57781206a712169f0583e0e23dd31c
PRTS <56300237+ESEAbsolute@users.noreply.github.com>
2026-03-10 01:21:48 +0800

    fix poke not shaking avatar

--- src/main/dispatcher.ts
+++ src/main/dispatcher.ts
@@ -3,19 +3,19 @@ import { PokeConfig, PokeMode } from "../config/types";
 import { Debug } from "../debug";
 import { NativePoke } from "../impl/nativepoke";
 
-export async function sendPoke(params: PokeParams) {
+export async function sendPoke(params: PokeParams): Promise<boolean> {
     var cfg: PokeConfig = PluginSettings.main.readConfig("qwqnt-poke", new PokeConfig());
     switch (cfg.mode) {
         case PokeMode.NATIVE:
-            NativePoke.doPoke(params);
-            break;
+            return NativePoke.doPoke(params);
         case PokeMode.NAPCAT:
             Debug.warn("MAIN/Dispatcher", "NapCat poke method not implemented");
-            break;
+            return false;
         case PokeMode.HTTP_API:
             Debug.warn("MAIN/Dispatcher", "HTTP_API poke method not implemented");
-            break;
+            return false;
         default:
             Debug.error("MAIN/Dispatcher", "Unknown PokeMode:", cfg.mode);
+            return false;
     }
 }
